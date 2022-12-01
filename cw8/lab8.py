3c231003adc7fe733ce68a8876eb9e1f48ab0868
Daniel Kucharski <danku@onet.eu>
2022-12-01 22:25:48 +0100

    commit

--- cw8/lab8.py
+++ cw8/lab8.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageFilter
 import numpy as np
 from PIL import ImageChops, ImageOps, ImageShow
 from PIL import ImageStat as stat
@@ -41,7 +41,7 @@ def histogram_equalization(im):
 im = Image.open("rtg.jpg")
 print(im.mode)
 szary = im.convert("L")
-statystyki(szary)
+#statystyki(szary)
 
 hist_norm = histogram_norm(im)[0]
 plt.title("histogram_norm")
@@ -59,28 +59,62 @@ plt.show()
 
 im_eq = histogram_equalization(im)
 im_eq.save("equalized.jpg")
-im_eq.show()
+#im_eq.show()
 
 im_eq2 = ImageOps.equalize(im, mask=None)
-im_eq2.show()
+im_eq2.save("equalized1.jpg")
+#im_eq2.show()
 
 
 hist_eq = im_eq.histogram()
 hist_eq2 = im_eq2.histogram()
-plt.figure(figsize=(64, 32))
-plt.subplot(1, 1, 1)
-plt.imshow(im_eq)
-plt.subplot(1, 1, 2)
-plt.imshow(im_eq2)
+plt.figure(figsize=(16, 8))
 plt.subplot(2, 2, 1)
+plt.title("im_eq")
 plt.bar(range(256), hist_eq[:256], color='r', alpha=0.5)
 plt.bar(range(256), hist_eq[256:2 * 256], color='g', alpha=0.4)
 plt.bar(range(256), hist_eq[2 * 256:], color='b', alpha=0.3)
 plt.subplot(2, 2, 2)
+plt.title("im_eq2")
 plt.bar(range(256), hist_eq2[:256], color='r', alpha=0.5)
 plt.bar(range(256), hist_eq2[256:2 * 256], color='g', alpha=0.4)
 plt.bar(range(256), hist_eq2[2 * 256:], color='b', alpha=0.3)
+plt.subplot(2, 2, 3)
+plt.axis("off")
+plt.imshow(im_eq)
+plt.subplot(2, 2, 4)
+plt.axis("off")
+plt.imshow(im_eq2)
 plt.show()
+
+print("STATYSTYKI IMAGEOPS")
+statystyki(im_eq2)
+print("\nSTATYSTYKI EQUALIZATION")
+statystyki(im_eq)
+
+im_detail = im.filter(ImageFilter.DETAIL)
+im_sharpen = im.filter(ImageFilter.SHARPEN)
+im_contour = im.filter(ImageFilter.CONTOUR)
+
+plt.subplot(1, 4, 1)
+plt.title("DETAIL")
+plt.axis("off")
+plt.imshow(im_detail)
+plt.subplot(1, 3, 2)
+plt.title("SHARPEN")
+plt.axis("off")
+plt.imshow(im_sharpen)
+plt.subplot(1, 3, 3)
+plt.title("CONTOUR")
+plt.axis("off")
+plt.imshow(im_contour)
+plt.title("EQUALIZED")
+plt.axis("off")
+plt.imshow(im_eq)
+plt.savefig("filtry.jpg")
+plt.show()
+
+
 
 
 
